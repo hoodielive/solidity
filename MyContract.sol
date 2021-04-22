2cafa1dae8154a24ff028676e9a985bef7ce27c1
hoodielive <hoodielive@gmail.com>
2021-04-22 01:24:07 -0400

    Solidity.

---
+++ MyContract.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.4;
+
+contract MyContract {
+   string value;
+}
+
+function get() public view returns(string) {
+    value = _value;
+    return value;
+}
+
+
---
+++ teen.sol
@@ -0,0 +1,23 @@
+uint x;
+
+int constant a = 8;
+int256 constant a = 8;
+uint constant VERSION_ID = 0x123A1;
+
+int256 public a = 8;
+uint8 b;
+int64 c;
+uint248 e;
+
+uint256 c = a + b;
+assert(c >= a);
+
+int x = int(b);
+bool b = true;
+
+address public owner;
+
+owner.transfer(SOME_BALANCE);
+
+if (owner.send) {}
+owner.balance;
